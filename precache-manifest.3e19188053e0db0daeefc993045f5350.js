0d2e796b7f9c6c5dd4271b280742b5d70d0be3f1
ABHINEET4 <108821830+ABHINEET4@users.noreply.github.com>
2024-02-13 20:08:33 +0530

    Updates

---
+++ precache-manifest.3e19188053e0db0daeefc993045f5350.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8753bd4d8747483d952e4789b992849",
+    "url": "/NewsView-Application/index.html"
+  },
+  {
+    "revision": "a76e5ccda4ebb1ea71de",
+    "url": "/NewsView-Application/static/css/3.07102e08.chunk.css"
+  },
+  {
+    "revision": "c85c781e46f6abf0dfa7",
+    "url": "/NewsView-Application/static/css/main.906c67b7.chunk.css"
+  },
+  {
+    "revision": "153215a2fe7b23731fd4",
+    "url": "/NewsView-Application/static/js/0.23487108.chunk.js"
+  },
+  {
+    "revision": "c47fb89f944fc413937f1d857df6495a",
+    "url": "/NewsView-Application/static/js/0.23487108.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a76e5ccda4ebb1ea71de",
+    "url": "/NewsView-Application/static/js/3.cdc5d1f9.chunk.js"
+  },
+  {
+    "revision": "ec6378bf61c787815d098c330adf52c5",
+    "url": "/NewsView-Application/static/js/3.cdc5d1f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a92f4ed5a750c669705b",
+    "url": "/NewsView-Application/static/js/4.bcd42b79.chunk.js"
+  },
+  {
+    "revision": "c290b88054a57ea68b08",
+    "url": "/NewsView-Application/static/js/5.f63f2cf2.chunk.js"
+  },
+  {
+    "revision": "c85c781e46f6abf0dfa7",
+    "url": "/NewsView-Application/static/js/main.48e44bf8.chunk.js"
+  },
+  {
+    "revision": "55057ec7a968eb7ad367",
+    "url": "/NewsView-Application/static/js/runtime-main.d392cb96.js"
+  }
+]);
